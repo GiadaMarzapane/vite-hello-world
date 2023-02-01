9de48d85f65c55ecc2d93294889489aa5f8ccbfe
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-01 18:13:53 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        {{ message }}
+    </h1>
+</template>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+</style>
